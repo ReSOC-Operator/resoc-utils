fc8fc79894a02ca4b73bfb86885cbc74f9631488
ReSOC-Operator <113511766+ReSOC-Operator@users.noreply.github.com>
2026-02-19 17:10:28 +0100

    Add files via upload

--- install_r7.sh
+++ install_r7.sh
@@ -1,5 +1,22 @@
-wget https://eu.storage.endpoint.ingress.rapid7.com/public.razor-prod-0.eu-central-1.insight.rapid7.com/endpoint/agent/1770062320/linux/x86_64/rapid7-insight-agent_4.0.21.45-1_amd64.deb
-apt-get install ./rapid7-insight-agent_4.0.21.45-1_amd64.deb -y
+#!/bin/sh
+
+URL="https://eu.storage.endpoint.ingress.rapid7.com/public.razor-prod-0.eu-central-1.insight.rapid7.com/endpoint/agent/1770062320/linux/x86_64/rapid7-insight-agent_4.0.21.45-1_amd64.deb"
+OUTPUT="/tmp/r7.deb"
+
+if command -v curl >/dev/null 2>&1; then
+    echo "curl trovato, avvio download..."
+    curl -fsSL "$URL" -o "$OUTPUT"
+
+    if [ $? -eq 0 ]; then
+        echo "Download completato: $OUTPUT"
+    else
+        echo "Errore durante il download"
+    fi
+else
+    echo "curl non presente"
+fi
+
+apt-get install /tmp/r7.deb -y
 cd /opt/rapid7/ir_agent/components/insight_agent/4.0.21.45
 /opt/rapid7/ir_agent/components/insight_agent/4.0.21.45/configure_agent.sh --token=eu:ab23ad3a-68c9-4fe1-b448-1d5083739129 -v --start
 systemctl status ir_agent.service
