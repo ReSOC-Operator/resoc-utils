b6a55c5c0d823851c4df071d6f437691b52e7d2f
ReSOC-Operator <113511766+ReSOC-Operator@users.noreply.github.com>
2026-02-19 15:57:37 +0100

    Add files via upload

---
+++ install_r7.sh
@@ -0,0 +1,5 @@
+wget https://eu.storage.endpoint.ingress.rapid7.com/public.razor-prod-0.eu-central-1.insight.rapid7.com/endpoint/agent/1770062320/linux/x86_64/rapid7-insight-agent_4.0.21.45-1_amd64.deb
+apt-get install ./rapid7-insight-agent_4.0.21.45-1_amd64.deb
+cd /opt/rapid7/ir_agent/components/insight_agent/4.0.21.45
+/opt/rapid7/ir_agent/components/insight_agent/4.0.21.45/configure_agent.sh --token=eu:d7bfca37-6f50-4382-b090-ff99d144d072 -v --start
+systemctl status ir_agent.service
